23a9e07ca30e29f4d5d605dda70cd8cfafc54b3d
hamzahfauzy <hamzahfauzy97@gmail.com>
2023-11-29 00:54:10 +0700

    update dashboard

--- process/index.php
+++ process/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use Core\Page;
+use Modules\Crud\Libraries\Repositories\CrudRepository;
 
 Page::set_title("Dashboard");
 Page::setActive("dashboard.index");
@@ -16,4 +17,24 @@ Page::setBreadcrumbs([
     ],
 ]);
 
-return view('dashboard/views/index');
+// init table fields
+$tableName  = 'exam_schedules';
+$table      = tableFields($tableName);
+$fields     = $table->getFields();
+$module     = $table->getModule();
+$success_msg = get_flash_msg('success');
+$error_msg   = get_flash_msg('error');
+
+// get data
+$crudRepository = new CrudRepository($tableName);
+$crudRepository->setModule($module);
+// $data           = $crudRepository->get();
+
+if(isset($_GET['draw']))
+{
+    return $crudRepository->dataTable($fields);
+}
+
+Page::pushFoot("<script src='".asset('assets/crud/js/crud.js')."'></script>");
+
+return view('dashboard/views/index', compact('fields', 'tableName', 'success_msg', 'error_msg', 'crudRepository'));
--- views/index.php
+++ views/index.php
@@ -1,5 +1,34 @@
 <?php get_header() ?>
 <div>
-    <h4>Selamat Datang</h4>
+    <h4>Selamat Datang <?=auth()->name?></h4>
+</div>
+<div class="card">
+    <div class="card-header">
+        <b>Jadwal Ujian</b>
+    </div>
+    <div class="card-body">
+        <div class="table-responsive table-hover table-sales">
+            <table class="table table-bordered datatable-crud" style="width:100%">
+                <thead>
+                    <tr>
+                        <th width="20px">#</th>
+                        <?php 
+                        foreach($fields as $field): 
+                            $label = $field;
+                            if(is_array($field))
+                            {
+                                $label = $field['label'];
+                            }
+                            $label = _ucwords($label);
+                        ?>
+                        <th><?=$label?></th>
+                        <?php endforeach ?>
+                        <th class="text-right">
+                        </th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+    </div>
 </div>
 <?php get_footer() ?>
